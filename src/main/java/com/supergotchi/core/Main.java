4030fc4f75b8ca0251b4a6e5172618bbe718fea3
Andrea Capuano <andrealfie@gmail.com>
2014-10-05 20:56:07 +0200

    Fixed little bug with loading game

--- src/main/java/com/supergotchi/core/Main.java
+++ src/main/java/com/supergotchi/core/Main.java
@@ -1,13 +1,11 @@
 package com.supergotchi.core;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Game game;
         if(SaveLoadUtilities.savedGameExists()) game = new Game(SaveLoadUtilities.loadGotchi());
-        game = new Game(new Gotchi("GotcheeName"));
-        game.gameLoop();
+        else game = new Game(new Gotchi("GotcheeName"));
+        game.gameLoop();    
     }
 
 
